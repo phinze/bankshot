9bd04928271313d599e0a669c5152b00cd06a9a8
Paul Hinze <phinze@phinze.com>
2025-10-13 09:40:31 -0500

    fix: correct YAML indentation in home-manager config generation

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -230,20 +230,16 @@ in {
         log_level: ${cfg.daemon.logLevel}
         ssh_command: ssh
         ${optionalString cfg.monitor.enable ''
-          monitor:
-            portRanges:
-          ${lib.concatMapStrings (range: ''
-              - start: ${toString range.start}
-                end: ${toString range.end}
-            '')
-            cfg.monitor.portRanges}
-            ignoreProcesses:
-          ${lib.concatMapStrings (proc: ''
-              - ${proc}
-            '')
-            cfg.monitor.ignoreProcesses}
-            pollInterval: ${cfg.monitor.pollInterval}
-            gracePeriod: ${cfg.monitor.gracePeriod}
+        monitor:
+          portRanges:
+        ${lib.concatMapStrings (range: ''
+            - start: ${toString range.start}
+              end: ${toString range.end}
+        '') cfg.monitor.portRanges}  ignoreProcesses:
+        ${lib.concatMapStrings (proc: ''
+            - ${proc}
+        '') cfg.monitor.ignoreProcesses}  pollInterval: ${cfg.monitor.pollInterval}
+          gracePeriod: ${cfg.monitor.gracePeriod}
         ''}
         ${lib.concatStringsSep "\n" (lib.mapAttrsToList (
             name: value:
